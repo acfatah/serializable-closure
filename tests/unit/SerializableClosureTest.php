c35752adc5f5211919b813d06677273fa7eeaaee
Achmad F. Ibrahim <acfatah@gmail.com>
2016-01-22 08:53:11 +0800

    Refactor test

--- tests/unit/SerializableClosureTest.php
+++ tests/unit/SerializableClosureTest.php
@@ -10,46 +10,89 @@ class SerializableClosureTest extends \PHPUnit_Framework_TestCase
     {
         $greet = 'Hello';
 
-        $this->closure = function ($person) use ($greet) {
+        $closure = function ($person) use ($greet) {
             return $greet . " $person!";
         };
 
-        $this->serializable = new Acfatah\SerializableClosure\SerializableClosure($this->closure);
-        $this->serialized = serialize($this->serializable);
+        $this->closure = $closure;
+    }
+
+    public function getWrappedClosure()
+    {
+        return new Acfatah\SerializableClosure\SerializableClosure(
+            $this->closure
+        );
+    }
+
+    public function serializeClosure()
+    {
+        return serialize($this->getWrappedClosure());
+    }
+
+    public function unserializeClosure()
+    {
+        return unserialize($this->serializeClosure());
+    }
+
+    public function testSerialize()
+    {
+        // no \Exception thrown
+        $this->serializeClosure();
     }
 
     public function testUnserialize()
     {
-        $unserialized = unserialize($this->serialized);
+        $unserialized = $this->unserializeClosure();
+
         $this->assertNotFalse($unserialized);
         $this->assertTrue(is_callable($unserialized));
         $this->assertEquals('Hello world!', $unserialized('world'));
     }
 
-    public function testGetClosure()
+    public function testClosuresProduceSameResult()
     {
         $closure = $this->closure;
-        $serializable = unserialize($this->serialized);
-        $unserialized_closure = $serializable->getClosure();
-        $expected = new \ReflectionFunction($closure);
-        $actual = new \ReflectionFunction($unserialized_closure);
+        $unserialized_closure = $this->unserializeClosure();
+
+        $this->assertEquals(
+            $closure('world'),
+            $unserialized_closure('world')
+        );
+    }
+
+    public function testClosureHasSameParameters()
+    {
+        $expected = new \ReflectionFunction($this->closure);
+        $actual = new \ReflectionFunction(
+            $this->unserializeClosure()->getClosure()
+        );
+
+        $this->assertEquals(
+            $expected->getNumberOfRequiredParameters(),
+            $actual->getNumberOfRequiredParameters()
+        );
 
         $this->assertEquals(
             $expected->getNumberOfParameters(),
             $actual->getNumberOfParameters()
         );
-        $this->assertEquals(
-            $expected->getNumberOfRequiredParameters(),
-            $actual->getNumberOfRequiredParameters()
-        );
+
         $this->assertEquals(
             $expected->getParameters(),
             $actual->getParameters()
         );
+    }
+
+    public function testClosureHasSameStaticVariables()
+    {
+        $expected = new \ReflectionFunction($this->closure);
+        $actual = new \ReflectionFunction(
+            $this->unserializeClosure()->getClosure()
+        );
+
         $this->assertSame(
             $expected->getStaticVariables(),
             $actual->getStaticVariables()
         );
-        $this->assertEquals($closure('world'), $unserialized_closure('world'));
     }
 }
