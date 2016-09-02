44ef49d62bb858899da17dbcbfcb58009d482869
Achmad F. Ibrahim <acfatah@gmail.com>
2016-09-03 04:47:27 +0800

    Fix github url

--- src/Serializer.php
+++ src/Serializer.php
@@ -6,7 +6,7 @@
  * Redistributions of files must retain the above copyright notice.
  *
  * @copyright (c) 2016, Achmad F. Ibrahim
- * @link https://github.com/acfatah/container
+ * @link https://github.com/acfatah/serializable-closure
  * @license http://opensource.org/licenses/mit-license.php The MIT License (MIT)
  */
 
